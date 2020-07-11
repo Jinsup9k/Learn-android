3dad7da89247ecfdc2d02bdf6b7e8521445cc51a
Jinsup9k <01077717141p@gmail.com>
2020-07-11 19:02:16 +0900

    Mission06: ProgressBar

---
+++ Mission06/app/src/main/java/com/j1/mission06/MainActivity.java
@@ -0,0 +1,44 @@
+package com.j1.mission06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        seekBar = findViewById(R.id.seekBar);
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int num = seekBar.getProgress();
+                textView.setText(num+"");
+                progressBar.setProgress(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
