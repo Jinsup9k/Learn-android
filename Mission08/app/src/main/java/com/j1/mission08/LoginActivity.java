4c4adfe3450b4154c3f622e76f59ac830c3db0d4
Jinsup9k <01077717141p@gmail.com>
2020-07-16 18:46:21 +0900

    add Mission08

---
+++ Mission08/app/src/main/java/com/j1/mission08/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.j1.mission08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 101;
+
+    EditText usernameInput;
+    EditText passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                if (username.equals("")) {
+                    Toast.makeText(getApplicationContext(), "아이디 입력해라", Toast.LENGTH_LONG).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(getApplicationContext(), "비밀번호 입력해라", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                    intent.putExtra("username", username);
+                    intent.putExtra("password", password);
+
+                    startActivityForResult(intent, REQUEST_CODE_MENU);
+                }
+            }
+        });
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE_MENU) {
+            if (intent != null) {
+                String menu = intent.getStringExtra("menu");
+                String message = intent.getStringExtra("message");
+
+                Toast toast = Toast.makeText(getBaseContext(), "result code : " + resultCode + ", menu : " + menu + ", message : " + message, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+
+    }
+}
