c9a4d7a33d22c3a75b7415acd5ca996c63f66319
Jinsup9k <01077717141p@gmail.com>
2020-08-03 15:20:36 +0900

    :Mission09

---
+++ Mission09/app/src/main/java/org/j1/mission09/MainActivity.java
@@ -0,0 +1,14 @@
+package org.j1.mission09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
